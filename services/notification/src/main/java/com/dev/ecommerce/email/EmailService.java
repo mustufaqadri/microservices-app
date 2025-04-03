c5a350029032082e9b43c41af4d62f113d63f99c
Mustufa Qadri <mustufa.qadri@sleepnumber.com>
2025-04-03 19:18:14 +0500

    completed notification service

---
+++ services/notification/src/main/java/com/dev/ecommerce/email/EmailService.java
@@ -0,0 +1,87 @@
+package com.dev.ecommerce.email;
+
+import com.dev.ecommerce.kafka.order.Product;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_RELATED;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendPaymentSuccessEmail(String customerEmail, String customerName, BigDecimal amount, String orderReference) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, MULTIPART_MODE_RELATED, UTF_8.name());
+        helper.setFrom("contact@dev.com");
+
+        final String templateName = EmailTemplate.PAYMENT_CONFIRMATION.getTemplate();
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("amount", amount);
+        variables.put("customerEmail", customerEmail);
+
+        Context context = new Context();
+        context.setVariables(variables);
+        helper.setSubject(EmailTemplate.PAYMENT_CONFIRMATION.getSubject());
+
+        try {
+            String htmlTemplate = templateEngine.process(templateName, context);
+            helper.setText(htmlTemplate, true);
+            helper.setTo(customerEmail);
+            mailSender.send(mimeMessage);
+            log.info("INFO: Email sent successfully to customer " + customerEmail);
+        } catch (MessagingException e) {
+            log.warn("ERROR: Email sent successfully to customer " + customerEmail);
+        }
+    }
+
+    @Async
+    public void sendOrderConfirmationEmail(String customerEmail, String customerName, BigDecimal amount, String orderReference, List<Product> products) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, MULTIPART_MODE_RELATED, UTF_8.name());
+        helper.setFrom("contact@dev.com");
+
+        final String templateName = EmailTemplate.ORDER_CONFIRMATION.getTemplate();
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("total amount", amount);
+        variables.put("products", products);
+        variables.put("customerEmail", customerEmail);
+
+        Context context = new Context();
+        context.setVariables(variables);
+        helper.setSubject(EmailTemplate.ORDER_CONFIRMATION.getSubject());
+
+        try {
+            String htmlTemplate = templateEngine.process(templateName, context);
+            helper.setText(htmlTemplate, true);
+            helper.setTo(customerEmail);
+            mailSender.send(mimeMessage);
+            log.info("INFO: Email sent successfully to customer " + customerEmail);
+        } catch (MessagingException e) {
+            log.warn("ERROR: Email sent successfully to customer " + customerEmail);
+        }
+    }
+}
--- services/notification/src/main/java/com/dev/ecommerce/kafka/NotificationConsumer.java
+++ services/notification/src/main/java/com/dev/ecommerce/kafka/NotificationConsumer.java
@@ -1,10 +1,12 @@
 package com.dev.ecommerce.kafka;
 
+import com.dev.ecommerce.email.EmailService;
 import com.dev.ecommerce.kafka.order.OrderConfirmation;
 import com.dev.ecommerce.kafka.payment.PaymentConfirmation;
 import com.dev.ecommerce.notification.Notification;
 import com.dev.ecommerce.notification.NotificationRepository;
 import com.dev.ecommerce.notification.NotificationType;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -18,10 +20,10 @@ import java.time.LocalDateTime;
 public class NotificationConsumer {
 
     private final NotificationRepository repository;
-    // private final EmailService;
+    private final EmailService emailService;
 
     @KafkaListener(topics = "payment-topic")
-    public void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation) {
+    public void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation) throws MessagingException {
         log.info(String.format("Consuming the message from payment-topic Topic:: %s", paymentConfirmation));
         repository.save(
                 Notification.builder()
@@ -31,12 +33,18 @@ public class NotificationConsumer {
                         .build()
         );
 
-        // TODO send email
+        var customerName = paymentConfirmation.customerFirstName() + " " + paymentConfirmation.customerLastName();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.customerEmail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+        );
     }
 
     @KafkaListener(topics = "order-topic")
-    public void consumeOrderConfirmationNotification(OrderConfirmation orderConfirmation) {
-        log.info(String.format("Consuming the message from order-topic Topic:: %s", paymentConfirmation));
+    public void consumeOrderConfirmationNotification(OrderConfirmation orderConfirmation) throws MessagingException {
+        log.info(String.format("Consuming the message from order-topic Topic:: %s", orderConfirmation));
         repository.save(
                 Notification.builder()
                         .type(NotificationType.ORDER_CONFIRMATION)
@@ -45,7 +53,14 @@ public class NotificationConsumer {
                         .build()
         );
 
-        // TODO send email
+        var customerName = orderConfirmation.customer().firstName() + " " + orderConfirmation.customer().lastName();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+        );
     }
 
 }
